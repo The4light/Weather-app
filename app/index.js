c6b816d4ee28f8e0b25a0e3726d040ae27735262
The4light <ddogbonna08@gmail.com>
2026-02-17 00:41:01 +0100

    made updates to format page and about to begin api implementation

--- app/index.js
+++ app/index.js
@@ -155,8 +155,73 @@ export default function HomeScreen() {
                         contentContainerStyle={{paddingHorizontal: 15}}
                         showsHorizontalScrollIndicator={false}                    
                     >
-                        <View>
-                            
+                        {/* FORECAST SPAM */}
+                        <View
+                            className="flex justify-center items-center w-24 rounded-xl py-3 space-y-1 mr-4 bg-[rgba(75,85,99,0.6)]"
+                            // style={{backgroundColor: theme.bgWhite(0.15)}}
+                        >
+                            <Image source={require("../assets/images/storm.png")} 
+                                className="h-11 w-11"
+                            />
+                            <Text className="text-white">Monday</Text>
+                            <Text className="text-white text-xl font-semibold">
+                                13℃
+                            </Text>
+                        </View>
+
+                        <View
+                            className="flex justify-center items-center w-24 rounded-xl py-3 space-y-1 mr-4 bg-[rgba(75,85,99,0.6)]"
+                            // style={{backgroundColor: theme.bgWhite(0.15)}}
+                        >
+                            <Image source={require("../assets/images/storm.png")} 
+                                className="h-11 w-11"
+                            />
+                            <Text className="text-white">Monday</Text>
+                            <Text className="text-white text-xl font-semibold">
+                                1℃
+                            </Text>
+                        </View>
+
+                        {/* SPAM 2 */}
+                        <View
+                            className="flex justify-center items-center w-24 rounded-xl py-3 space-y-1 mr-4 bg-[rgba(75,85,99,0.6)]"
+                            // style={{backgroundColor: theme.bgWhite(0.15)}}
+                        >
+                            <Image source={require("../assets/images/storm.png")} 
+                                className="h-11 w-11"
+                            />
+                            <Text className="text-white">Monday</Text>
+                            <Text className="text-white text-xl font-semibold">
+                                1℃
+                            </Text>
+                        </View>
+
+                          {/* SPAM 3 */}
+                        <View
+                            className="flex justify-center items-center w-24 rounded-xl py-3 space-y-1 mr-4 bg-[rgba(75,85,99,0.6)]"
+                            // style={{backgroundColor: theme.bgWhite(0.15)}}
+                        >
+                            <Image source={require("../assets/images/storm.png")} 
+                                className="h-11 w-11"
+                            />
+                            <Text className="text-white">Monday</Text>
+                            <Text className="text-white text-xl font-semibold">
+                                1℃
+                            </Text>
+                        </View>
+
+                              {/* SPAM 3 */}
+                        <View
+                            className="flex justify-center items-center w-24 rounded-xl py-3 space-y-1 mr-4 bg-[rgba(75,85,99,0.6)]"
+                            // style={{backgroundColor: theme.bgWhite(0.15)}}
+                        >
+                            <Image source={require("../assets/images/storm.png")} 
+                                className="h-11 w-11"
+                            />
+                            <Text className="text-white">Monday</Text>
+                            <Text className="text-white text-xl font-semibold">
+                                1℃
+                            </Text>
                         </View>
                     </ScrollView>
                 </View>
